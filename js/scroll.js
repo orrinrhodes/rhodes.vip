b4e82883893be57f40c88ef7e44550f5dedb13cb
orrin <44979097+orrinrhodes@users.noreply.github.com>
2021-12-24 02:51:47 -0500

    eya

--- js/scroll.js
+++ js/scroll.js
@@ -1,13 +1,17 @@
 played = false;
+ 
+window.onscroll = function() {myFunction()};
 
-$(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-    var zoomscroll = $('.zoom');
-    //console.log(scroll,zoomscroll)
+function myFunction() {
     if (!played) {
         var sigma = new Audio('./audio/sigma.mp3');
         sigma.play();
         sigma.autoplay = true;
         played = true;
     }
-});
+    var max = document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (max/height) * 100;
+    document.getElementById("line").style.height = scrolled + "%";
+    console.log(scrolled);
+};
