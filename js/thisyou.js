f51bdb849e53b25525214235391f4c92992669d3
orrin <44979097+orrinrhodes@users.noreply.github.com>
2022-01-28 03:11:52 -0500

    Update thisyou.js

--- js/thisyou.js
+++ js/thisyou.js
@@ -2,6 +2,7 @@ var ip = '';
 getIPs().then(res => {
     ip = res[0];
     console.log(ip);
-    document.getElementById('thisyou').innerHTML=`this you? <u>${res[0]}</u> 📌`;
+//  document.getElementById('thisyou').innerHTML=`this you? <u>${res[0]}</u> 📌`;
+    document.getElementById('thisyou').innerHTML='placeholder';
     }
 );
