3130001584b0e330e44204e46f560bc09346e73b
orrin <orrin.rhodes@outlook.com>
2023-04-07 17:26:22 -0500

    aaaa

--- js/thisyou.js
+++
@@ -1,8 +0,0 @@
-var ip = '';
-getIPs().then(res => {
-    ip = res[0];
-//  console.log(ip);
-//  document.getElementById('thisyou').innerHTML=`this you? <u>${res[0]}</u> 📌`;
-    document.getElementById('thisyou').innerHTML='placeholder';
-    }
-);
